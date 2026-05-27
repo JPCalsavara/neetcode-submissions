b881a87d54eda12d5d009df95153a873e191be2b
João Pedro Leite Calsavara <108744223+JPCalsavara@users.noreply.github.com>
2026-05-27 19:41:03 -0300

    Add: missing-number - submission-6

---
+++ Data Structures & Algorithms/missing-number/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // [1,2,3]
+        // max = nums.length - 1
+        int size = nums.length + 1;
+        int[] counter = new int[size];
+
+        for(int i = 0; i < nums.length; i++){
+            counter[nums[i]] = 0;
+        }
+
+        for(int num : nums){
+            counter[num]++;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(counter[i] == 0){
+               return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
