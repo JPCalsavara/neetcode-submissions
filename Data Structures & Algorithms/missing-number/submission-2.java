4201130da4d18380899eabd0f36a86c1116c9956
João Pedro Leite Calsavara <108744223+JPCalsavara@users.noreply.github.com>
2026-05-27 19:30:33 -0300

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // [1,2,3]
+        // max = nums.length - 1
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            if(i == nums[i]){
+               continue;
+            } else{
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
